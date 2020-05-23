4720ce1ce391d79ec1c8a549c383e4bd057283c8
Richard Weickelt <richard@weickelt.de>
2020-05-23 20:48:05 +0200

    Move data to the yml file
    This keeps the conan recipe tidy and clean.

--- recipes/qbs/all/conanfile.py
+++ recipes/qbs/all/conanfile.py
@@ -1,5 +1,5 @@
 from conans import ConanFile, tools
-import os
+from pathlib import Path
 
 
 class QbsConan(ConanFile):
@@ -14,70 +14,23 @@ class QbsConan(ConanFile):
         "os": ["Linux", "Macos", "Windows"]
     }
 
+    @property
+    def _data(self):
+        return self.conan_data['sources'][self.version][str(self.settings.os)]
+
     def source(self):
         pass
 
     def build(self):
-        data = dict(**self.conan_data["sources"][self.version][str(self.settings.os_build)])
-        tools.download(**data, filename="creator.7z")
+        tools.download(url = self._data['url'], sha256 = self._data['sha256'],
+                       filename = "creator.7z")
+
         self.run("cmake -E tar x creator.7z")
 
     def package(self):
-        if self.settings.os_build == "Linux":
-            self.copy("bin/qbs*", symlinks=True)
-            self.copy("bin/qt.conf", symlinks=True)
-            self.copy("lib/qtcreator/libqbs*", symlinks=True)
-            self.copy("lib/qtcreator/plugins/qbs/*", symlinks=True)
-            self.copy("lib/Qt/lib/libic*", symlinks=True)
-            self.copy("lib/Qt/lib/libQt5Core*", symlinks=True)
-            self.copy("lib/Qt/lib/libQt5DBus*", symlinks=True)
-            self.copy("lib/Qt/lib/libQt5Gui*", symlinks=True)
-            self.copy("lib/Qt/lib/libQt5Network*", symlinks=True)
-            self.copy("lib/Qt/lib/libQt5PrintSupport*", symlinks=True)
-            self.copy("lib/Qt/lib/libQt5Script*", symlinks=True)
-            self.copy("lib/Qt/lib/libQt5Widgets*", symlinks=True)
-            self.copy("lib/Qt/lib/libQt5Xml*", symlinks=True)
-            self.copy("lib/Qt/lib/libQt5XcbQpa*", symlinks=True)
-            self.copy("lib/Qt/plugins/platforms/libqxcb*", symlinks=True)
-            self.copy("lib/Qt/plugins/platformthemes/libqgtk3*", symlinks=True)
-            self.copy("libexec/qtcreator/qbs/*", symlinks=True)
-            self.copy("share/qtcreator/qbs/*", symlinks=True)
-
-        elif self.settings.os_build == "Macos":
-            self.copy("Qt Creator.app/Contents/MacOS/qbs*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/MacOS/qbs/*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Frameworks/libqbs*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Frameworks/QtCore*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Frameworks/QtDBus*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Frameworks/QtGui*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Frameworks/QtNetwork*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Frameworks/QtPrintSupport*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Frameworks/QtScript*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Frameworks/QtWidgets*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Frameworks/QtXml*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/PlugIns/qbs/*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/PlugIns/platforms/*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/PlugIns/styles/*", symlinks=True)
-            self.copy("Qt Creator.app/Contents/Resources/qbs/*", symlinks=True)
-
-        elif self.settings.os_build == "Windows":
-            self.copy("bin/qbs*", symlinks=True)
-            self.copy("bin/Qt5Core*", symlinks=True)
-            self.copy("bin/Qt5DBus*", symlinks=True)
-            self.copy("bin/Qt5Gui*", symlinks=True)
-            self.copy("bin/Qt5Network*", symlinks=True)
-            self.copy("bin/Qt5PrintSupport*", symlinks=True)
-            self.copy("bin/Qt5Script*", symlinks=True)
-            self.copy("bin/Qt5Widgets*", symlinks=True)
-            self.copy("bin/Qt5Xml*", symlinks=True)
-            self.copy("bin/dw*", symlinks=True)
-            self.copy("lib/Qt/plugins/platforms/qwindows*", symlinks=True)
-            self.copy("lib/Qt/plugins/styles/qwindowsvistastyle*", symlinks=True)
+        for p in self._data['patterns']:
+            self.copy(p, symlinks=True)
 
     def package_info(self):
-        if self.settings.os_build == "Macos":
-            binpath = os.path.join(self.package_folder, "Qt Creator.app", "Contents", "MacOS")
-        else:
-            binpath = os.path.join(self.package_folder, "bin")
-
-        self.env_info.PATH.append(binpath)
+        binpath = Path(self.package_folder, self._data['binpath'])
+        self.env_info.PATH.append(str(binpath))
